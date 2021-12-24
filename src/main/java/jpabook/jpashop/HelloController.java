e31fe7b1d6ea88e42bce6aa649925e1ca8b172cd
DDDZZZ <dylee0568@gmail.com>
2021-12-24 22:52:51 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!!"); // data를 실어서 View로 넘기기
+        return "hello"; // View의 이름
+    }
+}
